d6059e659f79cbbb0362e4124b656c91c13c082a
Ken Blum <ken.blum@outlook.com>
2020-05-13 16:53:14 +0200

    update to @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { AngularFirestoreModule, FirestoreSettingsToken } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireModule } from "@angular/fire";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { environment } from 'src/environments/environment';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule,
+  imports: [
+    BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: FirestoreSettingsToken, useValue: {} }
+    IonicModule.forRoot(),
+    AppRoutingModule,
   ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,37 +1,41 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { Component } from "@angular/core";
+import { map } from "rxjs/operators";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
   rooms: Room[];
   savingRoomId: string;
 
-  constructor(public db: AngularFirestore) {
-    this.db
-      .collection<Room>('/rooms')
+  constructor(public firestore: AngularFirestore) {
+    this.firestore
+      .collection<Room>("/rooms")
       .snapshotChanges()
-      .pipe(map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as Room;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        });
-      }))
-      .subscribe(rooms => {
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data() as Room;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        })
+      )
+      .subscribe((rooms) => {
         this.rooms = rooms;
       });
   }
 
   async update(id: string, room: Room) {
-    const doc = this.db.collection<Room>('/rooms').doc<Room>(id);
+    const doc = this.firestore.collection<Room>("/rooms").doc<Room>(id);
     await doc.update({ occupancy: room.occupancy });
 
     this.savingRoomId = id;
-    setTimeout(() => { this.savingRoomId = null; }, 2000);
+    setTimeout(() => {
+      this.savingRoomId = null;
+    }, 2000);
   }
 }
